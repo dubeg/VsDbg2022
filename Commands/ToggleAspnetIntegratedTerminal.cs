8f5ffc54b2ad37391a337299fe267330adc8b816
Guillaume Dube <guillaumedube@hotmail.com>
2025-09-11 22:12:18 -0400

    Sync with 2026 version

---
+++ Commands/ToggleAspnetIntegratedTerminal.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using EnvDTE;
+using Microsoft.Internal.VisualStudio.Shell.Interop;
+
+namespace VsDbg.Commands;
+
+[Command(PackageIds.ToggleAspnetIntegratedTerminal)]
+internal sealed class ToggleAspnetIntegratedTerminal : BaseCommand<ToggleAspnetIntegratedTerminal> {
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e) => throw new NotImplementedException();
+}
