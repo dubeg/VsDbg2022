86f20f6c15be3fd3a9fe52004abb960b43481945
Guillaume Dube <guillaumedube@hotmail.com>
2025-09-11 23:56:52 -0400

    Rename symbol without the annoying adornment layer

--- Commands/RefactorRename.cs
+++ Commands/RefactorRename.cs
@@ -6,8 +6,16 @@
 /// </summary>
 [Command(PackageIds.RefactorRename)]
 internal sealed class RefactorRename : BaseCommand<RefactorRename> {
-    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e) { 
+
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e) {
         await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
         await VS.Commands.ExecuteAsync("Refactor.Rename");
+        var doc = await VS.Documents.GetActiveDocumentViewAsync();
+        // InlineRenameAdornmentProvider.AdornmentLayerName = "RoslynRenameDashboard"
+        // Those classes are defined in Microsoft.CodeAnalysis.EditorFeatures.dll
+        var layer = doc.TextView.GetAdornmentLayer("RoslynRenameDashboard");
+        if (layer is not null) {
+            layer.RemoveAllAdornments();
+        }
     }
 }
